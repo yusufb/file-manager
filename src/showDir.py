5e6d9f53e3e78d91f360a4e39f739eba7a24910b
autkusoytas <autkusoytas@gmail.com>
2014-04-08 09:02:20 -0430

    showDir func seperated from Main module

---
+++ src/showDir.py
@@ -0,0 +1,24 @@
+'''
+Created on Apr 8, 2014
+@author: utku
+'''
+from PyQt4 import QtCore,QtGui
+from genericpath import isdir
+import Main
+
+class showDir(Main.WindowSource):
+    newDir = ''
+    def showDirFunc(self):
+        if(isdir(self.newDir)):
+            self.root = self.fileSystemModel.setRootPath(self.newDir)
+            self.treeView.setModel(self.fileSystemModel)
+            self.treeView.setRootIndex(self.root)
+            return self.newDir
+            print "current dir is now " + self.currentDir
+        else:
+            print self.newDir + " is not a directory"
+            
+    def __init__(self, currentDir):
+        super(showDir, self).__init__(None)
+        self.newDir = currentDir
+        self.showDirFunc()
