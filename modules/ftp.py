af286804566d4a5e763fdb3d8073fdb34ae58769
yigitanil <yigitan@gmail.com>
2014-04-08 18:10:40 +0300

    upload and download methods added

--- modules/ftp.py
+++ modules/ftp.py
@@ -13,9 +13,32 @@ def ftpConnection(adress,userName,password):
         ftp.login(userName,password)
         lines= ftp.nlst()
         print lines
-        
+        return ftp
     except ftplib.all_errors as e:
         print('An error has occured during ftp connection:',e)
 
 
-ftpConnection("ftp.ygt.hol.es", "u255885439.pyhton", "123456")
+
+def uploadFile(ftpConn,filePath,fileName):
+    import ftplib
+    try:
+        ftp=ftpConn
+        file = open(filePath,'rb')                  # file to send
+        ftp.storbinary('STOR '+fileName, file)  
+        print "Upload succesful"
+    except ftplib.all_errors as e:
+        print('An error has occured during file upload:',e)    
+        
+def downloadFile(ftpConn,filePath,fileName):
+    import ftplib
+    try:
+        ftp=ftpConn
+        file = open(filePath,'wb').write              
+        ftp.retrbinary('RETR '+fileName, file)  
+        print "Download succesful"
+    except ftplib.all_errors as e:
+        print('An error has occured during file upload:',e)        
+        
+            
+downloadFile(ftpConnection("ftp.ygt.hol.es", "u255885439.pyhton", "123456"),"C:\\Users\ANIL\\Desktop\\order.xml","order.xml")
+  
