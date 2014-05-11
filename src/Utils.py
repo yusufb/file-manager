4aa4aabc5eb255603360e1cefb3532ff088f8baa
yusufb <drukveyul@gmail.com>
2014-05-11 23:47:17 +0300

    extension check corrected

--- src/Utils.py
+++ src/Utils.py
@@ -9,7 +9,7 @@ def getParentDir(currentDir):
 
 def getFileExtension(fileName):
     ext = fileName.rsplit('.')[-1]
-    if len(ext)>0 and len(fileName.rsplit('.',1)[0])>0:
+    if len(ext)>0 and len(fileName.rsplit('.',1)[0])>0 and "." in fileName:
         return ext
     else:
         return False
