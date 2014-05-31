2400e882da3b91d40bbd3c4fecb32fe3971ba9c4
yusufb <drukveyul@gmail.com>
2014-05-31 20:09:56 +0300

    added seperators to right click menu

--- src/Main.py
+++ src/Main.py
@@ -64,7 +64,6 @@ class WindowSource(QtGui.QMainWindow,design.Ui_Dialog):
         self.bookmarkButton.triggered.connect(self.callAddToBookmarks)
         self.bookmarkListButton.triggered.connect(self.callListBookmarks)
         self.ftpConnectionButton.triggered.connect(self.callFtp)
-        self.createTagButton.triggered.connect(self.callCreateTag)
         
         self.treeView.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.treeView.customContextMenuRequested.connect(self.rightClickMenu)
@@ -95,13 +94,17 @@ class WindowSource(QtGui.QMainWindow,design.Ui_Dialog):
         print "right clicked"
         
         menu = QtGui.QMenu()
-        actionsList = OrderedDict((('Open', 'callOpenFile'), ('Rename', 'callRename'), ('Copy', 'copyFile'), ('Cut' , 'cutFile'), ('Paste', 'pasteFile'), ('Delete', 'callDelete'), ('File Type Info', 'callFileTypeInfo'), ('Add to Bookmarks', 'callAddToBookmarks'), ('Add Tags', 'callAddToTags'), ('Properties', 'callProperties')))
+        actionsList = OrderedDict((('Open', 'callOpenFile'),  ('Copy', 'copyFile'), ('Cut' , 'cutFile'), ('Paste', 'pasteFile'), ('Rename', 'callRename'), ('Delete', 'callDelete'),  ('Add to Bookmarks', 'callAddToBookmarks'), ('Add to Tag', 'callAddToTag'), ('File Type Info', 'callFileTypeInfo'), ('Properties', 'callProperties')))
+        seperatorAfterThis = ['callOpenFile', 'pasteFile', 'callDelete', 'callAddToTag']
         actions = []
         actionFunctions = []
         
         for k,v in actionsList.iteritems():
+
             actions.append(menu.addAction(k))
             actionFunctions.append(v)
+            if v in seperatorAfterThis:
+                menu.addSeparator()
         
         action = menu.exec_(self.treeViews[self.activeTreeview].mapToGlobal(pos))
         
@@ -162,14 +165,8 @@ class WindowSource(QtGui.QMainWindow,design.Ui_Dialog):
         if(isfile(toOpenFile)):
             openFile.openFile(toOpenFile)
     
-    def callAddToTags(self):
-        import addToTags
-        addToTags.addToTags(self.currentDir + "/" + self.clickedFileOrDir)
-    
-    def callCreateTag(self):
-        import createTag
-        createTag.createTag(self.currentDir + "/" + self.clickedFileOrDir)
-    
+    def callAddTotag(self):
+        return
     
     def callProperties(self):
         import properties
