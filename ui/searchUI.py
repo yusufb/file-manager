2c51fa68013df0ec4acdece1a250c8d742edd188
autkusoytas <autkusoytas@gmail.com>
2014-06-03 10:53:20 -0430

    creating search form

---
+++ ui/searchUI.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'searchForm.ui'
+#
+# Created: Tue Jun 03 18:12:10 2014
+#      by: PyQt4 UI code generator 4.10.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+import sys
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(404, 257)
+        self.checkBox_3 = QtGui.QCheckBox(Form)
+        self.checkBox_3.setGeometry(QtCore.QRect(100, 90, 101, 17))
+        self.checkBox_3.setObjectName(_fromUtf8("checkBox_3"))
+        self.pushButton = QtGui.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(10, 210, 75, 23))
+        self.pushButton.setAutoDefault(True)
+        self.pushButton.setDefault(True)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.pushButton_2 = QtGui.QPushButton(Form)
+        self.pushButton_2.setGeometry(QtCore.QRect(100, 210, 75, 23))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.line = QtGui.QFrame(Form)
+        self.line.setGeometry(QtCore.QRect(90, 80, 3, 61))
+        self.line.setFrameShape(QtGui.QFrame.VLine)
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setObjectName(_fromUtf8("line"))
+        self.widget = QtGui.QWidget(Form)
+        self.widget.setGeometry(QtCore.QRect(10, 90, 69, 42))
+        self.widget.setObjectName(_fromUtf8("widget"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.widget)
+        self.verticalLayout.setMargin(0)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.checkBox_2 = QtGui.QCheckBox(self.widget)
+        self.checkBox_2.setObjectName(_fromUtf8("checkBox_2"))
+        self.verticalLayout.addWidget(self.checkBox_2)
+        self.checkBox = QtGui.QCheckBox(self.widget)
+        self.checkBox.setObjectName(_fromUtf8("checkBox"))
+        self.verticalLayout.addWidget(self.checkBox)
+        self.widget1 = QtGui.QWidget(Form)
+        self.widget1.setGeometry(QtCore.QRect(10, 150, 381, 22))
+        self.widget1.setObjectName(_fromUtf8("widget1"))
+        self.horizontalLayout = QtGui.QHBoxLayout(self.widget1)
+        self.horizontalLayout.setMargin(0)
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.label_2 = QtGui.QLabel(self.widget1)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.horizontalLayout.addWidget(self.label_2)
+        self.lineEdit_2 = QtGui.QLineEdit(self.widget1)
+        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+        self.horizontalLayout.addWidget(self.lineEdit_2)
+        self.widget2 = QtGui.QWidget(Form)
+        self.widget2.setGeometry(QtCore.QRect(10, 30, 381, 22))
+        self.widget2.setObjectName(_fromUtf8("widget2"))
+        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.widget2)
+        self.horizontalLayout_2.setMargin(0)
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.label = QtGui.QLabel(self.widget2)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.horizontalLayout_2.addWidget(self.label)
+        self.lineEdit = QtGui.QLineEdit(self.widget2)
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.horizontalLayout_2.addWidget(self.lineEdit)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(_translate("Form", "Form", None))
+        self.checkBox_3.setText(_translate("Form", "Exact Match", None))
+        self.pushButton.setText(_translate("Form", "Search", None))
+        self.pushButton_2.setText(_translate("Form", "Cancel", None))
+        self.checkBox_2.setText(_translate("Form", "Directory", None))
+        self.checkBox.setText(_translate("Form", "File", None))
+        self.label_2.setText(_translate("Form", "Search in", None))
+        self.label.setText(_translate("Form", "Search ", None))
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    Dialog = QtGui.QDialog()
+    ui = Ui_Form()
+    ui.setupUi(Dialog)
+
+    Dialog.setWindowFlags(QtCore.Qt.WindowMinMaxButtonsHint)
+    Dialog.show()
+    Dialog.setFixedHeight(Dialog.height())
+    Dialog.setFixedWidth(Dialog.width())
+    print "dialog ui is created"
+    
+    sys.exit(app.exec_())
